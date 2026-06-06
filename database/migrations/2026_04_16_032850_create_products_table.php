64822c3eced44ae1a88d8e43970d73c1fdd3aa09
sArnas K <mufidsarnas123@gmail.com>
2026-06-06 17:48:16 +0700

    Tambahan rental price db

--- database/migrations/2026_04_16_032850_create_products_table.php
+++ database/migrations/2026_04_16_032850_create_products_table.php
@@ -4,25 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-   public function up(): void
-{
-    Schema::create('products', function (Blueprint $table) {
-        $table->id(); // This is your primary key
-        $table->string('name');
-        $table->text('description');
-        $table->string('image')->nullable();
-        $table->decimal('price', 10, 2);
-        $table->integer('rental_price')->nullable();
-        // habis hapus foreignid
-        $table->softDeletes();
-        $table->timestamps(); 
-    });
-}
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id(); // This is your primary key
+            $table->string('name');
+            $table->text('description');
+            $table->string('image')->nullable();
+            $table->decimal('price', 10, 2);
+            // habis hapus foreignid
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
 
     /**
      * Reverse the migrations.
