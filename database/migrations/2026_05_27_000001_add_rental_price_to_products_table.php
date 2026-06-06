76a685a74b3653c1994578411da46f7714154d47
Timotius adhi <timskuy28@gmail.com>
2026-06-06 18:21:57 +0700

    migration add rental price

--- database/migrations/2026_05_27_000001_add_rental_price_to_products_table.php
+++ database/migrations/2026_05_27_000001_add_rental_price_to_products_table.php
@@ -4,14 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->integer('rental_price')->nullable()->after('price');
+            $table->decimal('rental_price', 10, 2)->default(0)->after('price');
         });
     }
 
@@ -25,3 +26,4 @@ return new class extends Migration {
         });
     }
 };
+    
