3bf3a667a855a4c70b62c3102529aaf61edbdc9e
affirnanda <ahmadnanda402@gmail.com>
2026-06-21 20:00:09 +0700

    retesting cypress

--- cypress/e2e/TC-BF-1B.cy.js
+++ cypress/e2e/TC-BF-1B.cy.js
@@ -3,16 +3,16 @@ describe('TC-BF-1B: Admin mengunggah gambar dengan format salah', () => {
     cy.visit('/login');
     cy.get('input#email').type('super@admin.com');
     cy.get('input#password').type('admin123');
-    cy.get('button[type="submit"]').click();
+    cy.get('button[type="submit"]').click({force: true});
     cy.visit('/admin/kelola-hero-section');
-    cy.get('input[name="hero_title"]').clear().type('Solusi Terbaik');
-    cy.get('textarea[name="section_text"], input[name="section_text"]').clear().type('Deskripsi Hero');
+    cy.get('input[name="hero_title"]').clear({force: true}).type('Solusi Terbaik');
+    cy.get('textarea[name="section_text"], input[name="section_text"]').clear({force: true}).type('Deskripsi Hero', {force: true});
     cy.get('input[type="file"][name="hero_image"]').selectFile({
       contents: Cypress.Buffer.from('dummy pdf'),
       fileName: 'file.pdf',
       mimeType: 'application/pdf'
     });
-    cy.get('button[type="submit"]').click();
+    cy.get('button[type="submit"]').click({force: true});
     cy.screenshot('TC-BF-1B');
   });
 });
